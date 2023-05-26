4c278fefa9ed0cfad2501461e0fe570fbde9d379
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-05-26 08:55:28 +0200

    add emotion snipers

---
+++ src/components/user/user.jsx
@@ -0,0 +1,35 @@
+import { UserCard } from './user.styled';
+
+export const Profile = ({
+  userName,
+  tag,
+  location,
+  avatar = 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+  stats,
+}) => {
+  return (
+    <UserCard className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{userName}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </UserCard>
+  );
+};
